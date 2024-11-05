cd2e77587ef1b392e1adbd841d67e51391bc620b
WZD09 <2101210655@stu.pku.edu.cn>
2024-11-05 21:18:07 +0800

    feat: now fetch items from todo.api!!!

--- src/app/todo-item-list/todo-item-list.component.ts
+++ src/app/todo-item-list/todo-item-list.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Item } from '../models/item.model';
+import { ItemService } from '../item.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-todo-item-list',
   templateUrl: './todo-item-list.component.html',
   styleUrl: './todo-item-list.component.css',
 })
-export class TodoItemListComponent {
+export class TodoItemListComponent implements OnInit {
+  constructor(private itemService: ItemService) {}
+  todoItems = this.itemService.displayItems;
 
+  ngOnInit(): void {
+    this.itemService.getItems().subscribe(
+      (data) => {
+        this.itemService.setData(data);
+        this.todoItems = this.itemService.displayItems;
+        console.log('get data!', data);
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error, error.error?.message, error.message);
+      }
+    );
+  }
 }
--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Item } from '../models/item.model';
+import { ItemService } from '../item.service';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrl: './todo-item.component.css'
+  styleUrl: './todo-item.component.css',
 })
 export class TodoItemComponent {
+  constructor(private itemService: ItemService) {}
 
+  onDeleteClick() {
+      this.itemService.deleteItem(this.item.id);
+
+  }
+  @Input() item!: Item;
 }
